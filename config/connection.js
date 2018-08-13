f6fdafb426c9bca3138f5f54e0179d8c8e5afef7
Amitav Panda <36935650+amit8001@users.noreply.github.com>
2018-08-13 19:44:13 -0400

    edited connection.js file for remote deployment to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,34 @@
-// Set up MySQL connection.
+//Require mysql npm package
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "bapun",
-  database: "burger_db"
-});
+//Require dotenv package.
+require("dotenv").config();
 
-// Make connection.
+//use a remote db like db in Heroku
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+  //else use local db.
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL_PWD,
+    database: "burger_db"
+  });
+}
+
 connection.connect(function(err) {
+  //If there is an error when connecting to the database, log the error to the console.
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
+  //If a database connection is established, log the database thread number.
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+//Export the connection properties so that we can use them in other files.
 module.exports = connection;
